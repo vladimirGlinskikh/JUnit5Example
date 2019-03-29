b7c44840b838a1c5ef0c055f4255049ee4366782
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-29 16:47:00 +0600

    now I can create message in last parameter.

--- src/test/java/firstExample/AssertionsDemo.java
+++ src/test/java/firstExample/AssertionsDemo.java
@@ -3,13 +3,19 @@ package firstExample;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AssertionsDemo {
     private final Calculator calculator = new Calculator();
     private final Person person = new Person("Vladimir", "Glinskikh");
+
     @Test
-    void standartAssertions(){
+    void standartAssertions() {
         assertEquals(2, calculator.add(1, 1));
-        assertEquals(4, calculator.multiply(2, 2));
+        assertEquals(6, calculator.multiply(2, 3), "The optional failure message is now the last" +
+                "parameter");
+        assertTrue('a' < 'b', () -> "Assertion messages can be " +
+                "lazily evaluated --" +
+                "to avoid constructing complex messages unnecessarily.");
     }
 }
