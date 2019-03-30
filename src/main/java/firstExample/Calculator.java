340c6afef614c0983fcafb148b282c5b492d15c7
VladimirGlinskikh <support@zhelezyaka.kz>
2019-03-30 17:41:20 +0600

    method assertWithHamcrestMatcher() is green line. Check subtract.

--- src/main/java/firstExample/Calculator.java
+++ src/main/java/firstExample/Calculator.java
@@ -12,4 +12,8 @@ public class Calculator {
     public int divide(int a, int b) {
         return a / b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
---
+++ src/test/java/firstExample/HamcrestAssertionsDemo.java
@@ -0,0 +1,16 @@
+package firstExample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class HamcrestAssertionsDemo {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void assertWithHamcrestMatcher() {
+        assertThat(calculator.subtract(4, 1), is(equalTo(3)));
+    }
+}
